6441bc965730a9877963c02eb9679118cecfeb83
kir55rus <kir55rus@gmail.com>
2017-02-02 22:22:21 +0700

    Add NumberGuesser

---
+++ Batalin.Nsudotnet.NumberGuesser/NumberGuesser/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesser
+{
+    class Game
+    {
+        public int AttemptCount { get; private set; }
+
+        private readonly int _minNumber;
+        private readonly int _maxNumber;
+
+        private readonly Random _random = new Random();
+        private int _generatedNumber;
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        public TimeSpan GameDuration
+        {
+            get
+            {
+                return _endTime.Subtract(_startTime);
+            }
+        }
+
+        public Game(int minNumber, int maxNumber)
+        {
+            _minNumber = minNumber;
+            _maxNumber = maxNumber;
+        }
+
+        public void StartGame()
+        {
+            _generatedNumber = _random.Next(_minNumber, _maxNumber + 1);
+            AttemptCount = 0;
+            _startTime = DateTime.Now;
+        }
+
+        public void StopGame()
+        {
+            _endTime = DateTime.Now;
+        }
+
+        public AnswerResult TryAnswer(int answer)
+        {
+            ++AttemptCount;
+
+            if (_generatedNumber < answer)
+            {
+                return AnswerResult.Larger;
+            }
+            else if (_generatedNumber > answer)
+            {
+                return AnswerResult.Less;
+            }
+            else
+            {
+                return AnswerResult.Equal;
+            }
+        }
+
+        public enum AnswerResult
+        {
+            Less = 0,
+            Larger,
+            Equal
+        }
+    }
+}
---
+++ Batalin.Nsudotnet.NumberGuesser/NumberGuesser/JokeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    public class JokeManager
+    {
+        private const int JokesCount = 4;
+
+        private readonly string[] _jokes = new string[JokesCount];
+        private readonly Random _random = new Random();
+
+        public JokeManager(string name)
+        {
+            _jokes[0] = String.Format("Stone smarter than you, {0}", name);
+            _jokes[1] = String.Format("{0}, your intuition is bad", name);
+            _jokes[2] = String.Format("-_____-, who manage better than {0}? Each!", name);
+            _jokes[3] = String.Format("{0}, say you're kidding, please", name);
+        }
+
+        public string GetJoke()
+        {
+            int jokeIndex = _random.Next(_jokes.Length);
+            return _jokes[jokeIndex];
+        }
+    }
+}
