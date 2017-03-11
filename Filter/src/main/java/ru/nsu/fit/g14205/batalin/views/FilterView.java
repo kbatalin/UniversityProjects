61361e295ffbcc52550c8e5b048e7a221f374563
Kirill Batalin <kir55rus@gmail.com>
2017-03-11 19:10:41 +0700

    Add filter view

---
+++ Filter/src/main/java/ru/nsu/fit/g14205/batalin/views/FilterView.java
@@ -0,0 +1,12 @@
+package ru.nsu.fit.g14205.batalin.views;
+
+import ru.nsu.fit.g14205.batalin.controllers.FilterController;
+
+/**
+ * Created by kir55rus on 11.03.17.
+ */
+public class FilterView {
+    public FilterView(FilterController filterController) {
+
+    }
+}
