7a12af0b1339182457beb07f3690e12d003b9f58
Kirill Batalin <kir55rus@gmail.com>
2017-03-11 18:12:59 +0700

    Add start point

--- Filter/src/main/java/ru/nsu/fit/g14205/batalin/Filter.java
+++ Filter/src/main/java/ru/nsu/fit/g14205/batalin/Filter.java
@@ -1,7 +1,12 @@
 package ru.nsu.fit.g14205.batalin;
 
+import ru.nsu.fit.g14205.batalin.controllers.FilterController;
+
 /**
  * Created by kir55rus on 11.03.17.
  */
 public class Filter {
+    public static void main(String[] args) {
+        new FilterController().run();
+    }
 }
--- Filter/src/main/java/ru/nsu/fit/g14205/batalin/controllers/FilterController.java
+++ Filter/src/main/java/ru/nsu/fit/g14205/batalin/controllers/FilterController.java
@@ -4,4 +4,8 @@ package ru.nsu.fit.g14205.batalin.controllers;
  * Created by kir55rus on 11.03.17.
  */
 public class FilterController {
+
+    public void run() {
+
+    }
 }
