75a7760570fe23d512f1d6744cd9670903fbafa2
Kirill Batalin <kir55rus@gmail.com>
2017-03-18 00:20:49 +0700

    Add image model

---
+++ Filter/src/main/java/ru/nsu/fit/g14205/batalin/models/ImageModel.java
@@ -0,0 +1,33 @@
+package ru.nsu.fit.g14205.batalin.models;
+
+import ru.nsu.fit.g14205.batalin.utils.observe.Observable;
+import ru.nsu.fit.g14205.batalin.utils.observe.ObservableBase;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by kir55rus on 17.03.17.
+ */
+public class ImageModel extends ObservableBase implements Observable {
+    private BufferedImage image;
+
+    public ImageModel(BufferedImage image) {
+        this.image = image;
+    }
+
+    public ImageModel(File file) throws IOException {
+        image = ImageIO.read(file);
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public Image getImage(Rectangle area) {
+        return image.getSubimage(area.x, area.y, area.width, area.height);
+    }
+}
