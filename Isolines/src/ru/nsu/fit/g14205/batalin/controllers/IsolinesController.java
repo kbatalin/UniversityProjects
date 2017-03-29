0304e7e4994a384cb84cecb8d67a5ae9bd072616
Kirill Batalin <kir55rus@gmail.com>
2017-03-29 20:12:00 +0700

    Initial commit

---
+++ Isolines/src/ru/nsu/fit/g14205/batalin/controllers/IsolinesController.java
@@ -0,0 +1,11 @@
+package ru.nsu.fit.g14205.batalin.controllers;
+
+import java.io.InputStream;
+
+/**
+ * Created by kir55rus on 29.03.17.
+ */
+public class IsolinesController {
+    public void run() {
+    }
+}
---
+++ Isolines/src/ru/nsu/fit/g14205/batalin/views/IsolinesView.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.g14205.batalin.views;
+
+/**
+ * Created by kir55rus on 29.03.17.
+ */
+public class IsolinesView {
+}
