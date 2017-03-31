80bc434c5e36efb4570d5d9fd643b32aaf9ad410
Kirill Batalin <kir55rus@gmail.com>
2017-04-01 03:46:27 +0700

    Refactor view

--- Isolines/src/ru/nsu/fit/g14205/batalin/views/IsolinesView.java
+++ Isolines/src/ru/nsu/fit/g14205/batalin/views/IsolinesView.java
@@ -17,8 +17,7 @@ public class IsolinesView extends JFrame {
     private WorkspaceView workspaceView;
     private StatusBarView statusBarView;
 
-    private JMenuBar menuBar;
-    private JToolBar toolBar;
+    private ButtonsManager buttonsManager;
 
     public IsolinesView(IsolinesController isolinesController) {
         this.isolinesController = isolinesController;
@@ -30,183 +29,78 @@ public class IsolinesView extends JFrame {
 
         setLayout(new BorderLayout());
 
-        initMenu();
-
-        initToolbar();
-
         initWorkSpace();
-
         initStatusBar();
+        initButtons();
 
         setVisible(true);
     }
 
-    private void initMenu() {
-        menuBar = new JMenuBar();
+    private void initButtons() {
+        buttonsManager = new ButtonsManager();
+
+        buttonsManager.setMouseEnteredHandler(mouseEvent -> isolinesController.onEnterToolbarButton(mouseEvent));
+        buttonsManager.setMouseExitedHandler(mouseEvent -> isolinesController.onExitToolbarButton(mouseEvent));
 
         initFileMenu();
+        buttonsManager.addToolBarSeparator();
+
         initEditMenu();
+        buttonsManager.addToolBarSeparator();
+
         initViewMenu();
+        buttonsManager.addToolBarSeparator();
+
         initHelpMenu();
 
-        setJMenuBar(menuBar);
+        setJMenuBar(buttonsManager.getMenuBar());
+        add(buttonsManager.getToolBar(), BorderLayout.PAGE_START);
     }
 
     private void initFileMenu() {
-        JMenu fileMenu = new JMenu("File");
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        menuBar.add(fileMenu);
+        JMenu fileMenu = buttonsManager.addMenu(null, "File");
 
-        JMenuItem fileMenuNew = new JMenuItem("New");
-        fileMenuNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
-        fileMenuNew.setMnemonic(KeyEvent.VK_N);
-        fileMenu.add(fileMenuNew);
+        buttonsManager.addItem(fileMenu, "New", "Create new", "document.png", null);
+        buttonsManager.addItem(fileMenu, "Open", "Open", "folder.png", null);
+        buttonsManager.addItem(fileMenu, "Save", "Save", "save.png", null);
+        buttonsManager.addItem(fileMenu, "Save as...", "Save as...", "save_as.png", null);
 
-        JMenuItem fileMenuOpen = new JMenuItem("Open...");
-        fileMenuOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
-        fileMenuOpen.setMnemonic(KeyEvent.VK_O);
-        fileMenu.add(fileMenuOpen);
+        buttonsManager.addMenuSeparator(fileMenu);
 
-        JMenuItem fileMenuSave = new JMenuItem("Save");
-        fileMenuSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
-        fileMenuSave.setMnemonic(KeyEvent.VK_S);
-        fileMenu.add(fileMenuSave);
-
-        JMenuItem fileMenuSaveAs = new JMenuItem("Save as...");
-        fileMenuSaveAs.setMnemonic(KeyEvent.VK_A);
-        fileMenu.add(fileMenuSaveAs);
-
-        fileMenu.addSeparator();
-
-        JMenuItem fileMenuExit = new JMenuItem("Exit");
-        fileMenuExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
-        fileMenuExit.setMnemonic(KeyEvent.VK_X);
-        fileMenu.add(fileMenuExit);
+        buttonsManager.addItem(fileMenu, "Exit", null, null, null);
     }
 
     private void initEditMenu() {
-        JMenu editMenu = new JMenu("Edit");
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        menuBar.add(editMenu);
-
-        JMenuItem editMenuClear = new JMenuItem("Clear isolines");
-        editMenuClear.setMnemonic(KeyEvent.VK_B);
-        editMenu.add(editMenuClear);
+        JMenu editMenu = buttonsManager.addMenu(null, "Edit");
+        buttonsManager.addItem(editMenu, "Clear isolines", "Clear isolines", "trash.png", null);
     }
 
     private void initViewMenu() {
-        JMenu viewMenu = new JMenu("View");
-        viewMenu.setMnemonic(KeyEvent.VK_V);
-        menuBar.add(viewMenu);
+        JMenu viewMenu = buttonsManager.addMenu(null, "View");
 
-        JCheckBoxMenuItem viewMenuBlackWhite = new JCheckBoxMenuItem("Black/white");
-        viewMenu.add(viewMenuBlackWhite);
+        buttonsManager.addToggleItem(viewMenu, "Black/white", "Black/white", "map.png", false, null);
+        buttonsManager.addSeparator(viewMenu);
+        buttonsManager.addToggleItem(viewMenu, "Build type", "Build type", "picture.png", false, null);
+        buttonsManager.addSeparator(viewMenu);
+        buttonsManager.addToggleItem(viewMenu, "Mesh", "Mesh", "note.png", true, null);
+        buttonsManager.addToggleItem(viewMenu, "Map", "Map", "color.png", true, null);
+        buttonsManager.addToggleItem(viewMenu, "Isolines", "Isolines", "menu.png", true, null);
 
-        JCheckBoxMenuItem viewMenuBuildType = new JCheckBoxMenuItem("Build type");
-        viewMenu.add(viewMenuBuildType);
+        buttonsManager.addSeparator(viewMenu);
+        ButtonGroup menuGroup = new ButtonGroup();
+        ButtonGroup toolbarGroup = new ButtonGroup();
 
-        JCheckBoxMenuItem viewMenuMesh = new JCheckBoxMenuItem("Mesh");
-        viewMenu.add(viewMenuMesh);
-
-        JCheckBoxMenuItem viewMenuMap = new JCheckBoxMenuItem("Map");
-        viewMenu.add(viewMenuMap);
-
-        JCheckBoxMenuItem viewMenuIsolines = new JCheckBoxMenuItem("Isolines");
-        viewMenu.add(viewMenuIsolines);
-
-        viewMenu.addSeparator();
-
-        JMenuItem viewMenuZoomPlus = new JMenuItem("Zoom+");
-        viewMenu.add(viewMenuZoomPlus);
-
-        JMenuItem viewMenuZoomMinus = new JMenuItem("Zoom-");
-        viewMenu.add(viewMenuZoomMinus);
-
-        viewMenu.addSeparator();
-
-        ButtonGroup buttonGroup = new ButtonGroup();
-        JRadioButtonMenuItem viewMenuTriangleBarycentric = new JRadioButtonMenuItem("Triangle barycentric");
-        buttonGroup.add(viewMenuTriangleBarycentric);
-        viewMenu.add(viewMenuTriangleBarycentric);
-
-        JRadioButtonMenuItem viewMenuQuadBilinear = new JRadioButtonMenuItem("Quad bilinear");
-        buttonGroup.add(viewMenuQuadBilinear);
-        viewMenu.add(viewMenuQuadBilinear);
-
-        JRadioButtonMenuItem viewMenuTriangleBilinear = new JRadioButtonMenuItem("Triangle bilinear");
-        buttonGroup.add(viewMenuTriangleBilinear);
-        viewMenu.add(viewMenuTriangleBilinear);
+        buttonsManager.addRadioItem(viewMenu, "Triangle barycentric", menuGroup,
+                "Triangle barycentric", "number-one-in-a-circle.png", toolbarGroup, true, null);
+        buttonsManager.addRadioItem(viewMenu, "Quad bilinear", menuGroup,
+                "Quad bilinear", "number-two-in-a-circle.png", toolbarGroup, false, null);
+        buttonsManager.addRadioItem(viewMenu, "Triangle bilinear", menuGroup,
+                "Triangle bilinear", "number-three-in-a-circle.png", toolbarGroup, false, null);
     }
 
     private void initHelpMenu() {
-        JMenu helpMenu = new JMenu("Help");
-        helpMenu.setMnemonic(KeyEvent.VK_H);
-        menuBar.add(helpMenu);
-
-        JMenuItem helpMenuAbout = new JMenuItem("About");
-        helpMenuAbout.setMnemonic(KeyEvent.VK_A);
-        helpMenu.add(helpMenuAbout);
-    }
-
-    private void initToolbar() {
-        toolBar = new JToolBar("Toolbar");
-        toolBar.setFloatable(false);
-        add(toolBar, BorderLayout.PAGE_START);
-
-        addToolbarButton("Create new", "document.png", null);
-        addToolbarButton("Open", "folder.png", null);
-        addToolbarButton("Save", "save.png", null);
-        addToolbarButton("Save as...", "save_as.png", null);
-
-        toolBar.addSeparator();
-
-        addToolbarButton("Clear isolines", "trash.png", null);
-
-        toolBar.addSeparator();
-
-        addToolbarToggleButton("Black/white", "map.png", null);
-
-    }
-
-    private JButton addToolbarButton(String name, String icoName, ActionListener listener) {
-        JButton button = new JButton();
-        addToolbarButton(button, name, icoName, listener);
-        return button;
-    }
-
-    private JRadioButton addToolbarRadioButton(String name, String icoName, ButtonGroup buttonGroup, ActionListener listener) {
-        JRadioButton button = new JRadioButton();
-        buttonGroup.add(button);
-        addToolbarButton(button, name, icoName, listener);
-        return button;
-    }
-
-    private JToggleButton addToolbarToggleButton(String name, String icoName, ActionListener listener) {
-        JToggleButton button = new JToggleButton();
-        addToolbarButton(button, name, icoName, listener);
-        return button;
-    }
-
-    private void addToolbarButton(AbstractButton button, String name, String icoName, ActionListener listener) {
-        button.setName(name);
-        button.setToolTipText(name);
-        Icon aboutButtonIcon = getButtonIcon("images/" + icoName);
-        if (aboutButtonIcon != null) {
-            button.setIcon(aboutButtonIcon);
-        }
-        button.addActionListener(listener);
-        button.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent mouseEvent) {
-                isolinesController.onEnterToolbarButton(mouseEvent);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent mouseEvent) {
-                isolinesController.onExitToolbarButton(mouseEvent);
-            }
-        });
-        toolBar.add(button);
+        JMenu helpMenu = buttonsManager.addMenu(null, "Help");
+        buttonsManager.addItem(helpMenu, "About", "About", "info.png", null);
     }
 
     private void initWorkSpace() {
@@ -223,14 +117,5 @@ public class IsolinesView extends JFrame {
 
     public StatusBarView getStatusBarView() {
         return statusBarView;
-    }
-
-    private Icon getButtonIcon(String imgPath) {
-        URL imgUrl = this.getClass().getClassLoader().getResource("resources" + File.separator + imgPath);
-        if (imgUrl == null) {
-            return null;
-        }
-
-        return new ImageIcon(imgUrl);
     }
 }
