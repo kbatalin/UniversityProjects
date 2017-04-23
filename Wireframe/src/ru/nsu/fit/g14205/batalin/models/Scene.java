c523228ec7942f064da73ffaebd385947550b29d
Kirill Batalin <kir55rus@gmail.com>
2017-04-24 00:48:12 +0700

    add OutboardBox

--- Wireframe/src/ru/nsu/fit/g14205/batalin/models/Scene.java
+++ Wireframe/src/ru/nsu/fit/g14205/batalin/models/Scene.java
@@ -9,26 +9,50 @@ import java.util.List;
  */
 public class Scene implements Iterable<Segment> {
     private List<Segment> segments = new ArrayList<>();
+    private Parallelepiped outboardBox;
 
     public Scene() {
     }
 
     public Scene(List<Segment> segments) {
         this.segments.addAll(segments);
+        updOutboardBox();
     }
 
     public void addSegment(Segment segment) {
         segments.add(segment);
+        updOutboardBox();
     }
 
-    public
+    public Parallelepiped getOutboardBox() {
+        return outboardBox;
+    }
 
     public void clear() {
         segments.clear();
+        updOutboardBox();
     }
 
     public Segment getSegment(int index) {
         return segments.get(index);
+    }
+
+    private void updOutboardBox() {
+        if (segments.size() < 1) {
+            outboardBox = null;
+            return;
+        }
+
+        Segment firstSegment = segments.get(0);
+        Point3D min = firstSegment.getMinPoint();
+        Point3D max = firstSegment.getMaxPoint();
+
+        for (Segment segment : segments) {
+            min = Point3D.createMin(min, segment.getMinPoint());
+            max = Point3D.createMax(max, segment.getMaxPoint());
+        }
+
+        outboardBox = new Parallelepiped(min, max.getX() - min.getX(), max.getY() - min.getY(), max.getZ() - min.getZ());
     }
 
     @Override
