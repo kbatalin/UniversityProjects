bb9fa3a9d5f9a3d60f9b68558c6f31d5e989d63d
Kirill Batalin <kir55rus@gmail.com>
2017-04-24 00:20:34 +0700

    Add scene class

---
+++ Wireframe/src/ru/nsu/fit/g14205/batalin/models/Scene.java
@@ -0,0 +1,36 @@
+package ru.nsu.fit.g14205.batalin.models;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Kirill Batalin (kir55rus) on 23.04.17.
+ */
+public class Scene implements Iterable<Segment> {
+    private List<Segment> segments = new ArrayList<>();
+
+    public Scene() {
+    }
+
+    public Scene(List<Segment> segments) {
+        this.segments.addAll(segments);
+    }
+
+    public void addSegment(Segment segment) {
+        segments.add(segment);
+    }
+
+    public void clear() {
+        segments.clear();
+    }
+
+    public Segment getSegment(int index) {
+        return segments.get(index);
+    }
+
+    @Override
+    public Iterator<Segment> iterator() {
+        return segments.iterator();
+    }
+}
---
+++ Wireframe/src/ru/nsu/fit/g14205/batalin/models/Segment.java
@@ -0,0 +1,43 @@
+package ru.nsu.fit.g14205.batalin.models;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Created by Kirill Batalin (kir55rus) on 23.04.17.
+ */
+public class Segment implements Cloneable {
+    public Point2D first;
+    public Point2D second;
+
+    public Segment() {
+    }
+
+    public Segment(Point2D first, Point2D second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Point2D getFirst() {
+        return first;
+    }
+
+    public void setFirst(Point2D first) {
+        this.first = first;
+    }
+
+    public Point2D getSecond() {
+        return second;
+    }
+
+    public void setSecond(Point2D second) {
+        this.second = second;
+    }
+
+    @Override
+    public Segment clone() throws CloneNotSupportedException {
+        Segment segment = (Segment) super.clone();
+        segment.first = new Point2D.Double(first.getX(), first.getY());
+        segment.second = new Point2D.Double(second.getX(), second.getY());
+        return segment;
+    }
+}
