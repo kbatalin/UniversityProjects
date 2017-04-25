7968cf5426b5dfe5f52fb54069836a16d2921e55
Kirill Batalin <kir55rus@gmail.com>
2017-04-25 21:39:00 +0700

    Add observable in Grid

--- Wireframe/src/ru/nsu/fit/g14205/batalin/models/Grid.java
+++ Wireframe/src/ru/nsu/fit/g14205/batalin/models/Grid.java
@@ -1,11 +1,19 @@
 package ru.nsu.fit.g14205.batalin.models;
 
+import ru.nsu.fit.g14205.batalin.models.observe.Observable;
+import ru.nsu.fit.g14205.batalin.models.observe.ObservableBase;
+import ru.nsu.fit.g14205.batalin.models.observe.ObserveEvent;
+
 /**
  * Created by kir55rus on 12.04.17.
  */
-public class Grid {
-    public int cols;
-    public int rows;
+public class Grid extends ObservableBase implements Observable {
+    private int cols;
+    private int rows;
+
+    public enum Event implements ObserveEvent {
+        SIZE_CHANGED,
+    }
 
     public Grid(int cols, int rows) {
         if (!checkSize(cols) || !checkSize(rows)) {
@@ -14,6 +22,27 @@ public class Grid {
 
         this.cols = cols;
         this.rows = rows;
+    }
+
+    public Grid() {
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+        notifyObservers(Event.SIZE_CHANGED);
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+        notifyObservers(Event.SIZE_CHANGED);
     }
 
     public boolean checkSize(int value) {
