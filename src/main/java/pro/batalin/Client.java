68026e56df72728c9d94ef0c9973caa88bde5ead
Kirill Batalin <kir55rus@gmail.com>
2017-05-21 19:57:27 +0700

    Add main point

--- src/main/java/pro/batalin/Client.java
+++ src/main/java/pro/batalin/Client.java
@@ -1,7 +1,12 @@
 package pro.batalin;
 
+import pro.batalin.controllers.ClientController;
+
 /**
  * Created by Kirill Batalin (kir55rus).
  */
 public class Client {
+    public static void main(String[] args) {
+        new ClientController().run();
+    }
 }
--- src/main/java/pro/batalin/controllers/ClientController.java
+++ src/main/java/pro/batalin/controllers/ClientController.java
@@ -4,4 +4,7 @@ package pro.batalin.controllers;
  * Created by Kirill Batalin (kir55rus).
  */
 public class ClientController {
+    public void run() {
+
+    }
 }
