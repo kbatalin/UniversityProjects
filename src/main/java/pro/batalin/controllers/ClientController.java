c9205486a1c85ff6d56e297a89d42630b3657b27
Kirill Batalin <kir55rus@gmail.com>
2017-05-21 20:09:54 +0700

    Add login frame

--- src/main/java/pro/batalin/controllers/ClientController.java
+++ src/main/java/pro/batalin/controllers/ClientController.java
@@ -1,10 +1,23 @@
 package pro.batalin.controllers;
 
+import pro.batalin.models.properties.ApplicationProperties;
+import pro.batalin.models.properties.ApplicationPropertiesImpl;
+import pro.batalin.models.properties.LoginProperties;
+import pro.batalin.models.properties.LoginPropertiesImpl;
+
 /**
  * Created by Kirill Batalin (kir55rus).
  */
 public class ClientController {
-    public void run() {
+    private ApplicationProperties applicationProperties;
 
+    public void run() {
+        applicationProperties = new ApplicationPropertiesImpl(new LoginPropertiesImpl());
+
+        LoginController loginController = new LoginController();
+    }
+
+    public ApplicationProperties getApplicationProperties() {
+        return applicationProperties;
     }
 }
--- src/main/java/pro/batalin/controllers/LoginController.java
+++ src/main/java/pro/batalin/controllers/LoginController.java
@@ -1,7 +1,23 @@
 package pro.batalin.controllers;
 
+import pro.batalin.models.properties.LoginProperties;
+import pro.batalin.views.LoginFrame;
+
 /**
  * Created by Kirill Batalin (kir55rus).
  */
 public class LoginController {
+    private LoginProperties loginProperties;
+    private boolean result;
+
+    private LoginFrame loginFrame;
+
+    public LoginController(LoginProperties loginProperties) {
+        this.loginProperties = loginProperties;
+        result = false;
+    }
+
+    public void run() {
+        loginFrame = new LoginFrame(this);
+    }
 }
