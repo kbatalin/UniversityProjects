e2a83e6a8f4a444054384445f9f78969e5e00082
Kirill Batalin <kir55rus@gmail.com>
2017-06-03 19:01:52 +0700

    Add columnsView

---
+++ src/main/java/pro/batalin/views/workspaces/TableColumnView.java
@@ -0,0 +1,15 @@
+package pro.batalin.views.workspaces;
+
+import javax.swing.*;
+
+/**
+ * @author Kirill Batalin (kir55rus)
+ */
+public class TableColumnView extends JPanel {
+    private JPanel panel1;
+    private JTextField columnNameField;
+    private JTextField typeField;
+    private JTextField defaultValueFiled;
+    private JCheckBox notNullCheckBox;
+    private JCheckBox uniqueCheckBox;
+}
