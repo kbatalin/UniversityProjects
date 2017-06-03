f4d21c717184430cb1f2eabe3035769d0a4b06fc
Kirill Batalin <kir55rus@gmail.com>
2017-06-03 20:40:19 +0700

    Add creating columns

--- src/main/java/pro/batalin/views/workspaces/TableColumnView.java
+++ src/main/java/pro/batalin/views/workspaces/TableColumnView.java
@@ -1,15 +1,23 @@
 package pro.batalin.views.workspaces;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Kirill Batalin (kir55rus)
  */
 public class TableColumnView extends JPanel {
-    private JPanel panel1;
+    private JPanel contentPanel;
     private JTextField columnNameField;
     private JTextField typeField;
     private JTextField defaultValueFiled;
     private JCheckBox notNullCheckBox;
     private JCheckBox uniqueCheckBox;
+
+    public TableColumnView() {
+        setLayout(new BorderLayout());
+        add(contentPanel);
+
+        setMaximumSize(new Dimension(Integer.MAX_VALUE, getMinimumSize().height));
+    }
 }
--- src/main/java/pro/batalin/views/workspaces/TableCreatorView.java
+++ src/main/java/pro/batalin/views/workspaces/TableCreatorView.java
@@ -4,6 +4,11 @@ import pro.batalin.controllers.ClientController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kirill Batalin (kir55rus)
@@ -12,8 +17,19 @@ public class TableCreatorView extends WorkspaceBase {
     private JPanel contentPanel;
     private JTabbedPane tabbedPane;
     private JTextField tableNameField;
+    private JScrollPane columnsScrollPane;
     private JPanel columns;
+    private JButton executeButton;
+    private JButton cancelButton;
     private ClientController clientController;
+
+    private TableColumnView selectedColumn = null;
+
+    private JPopupMenu columnsPopupMenu;
+    private JMenuItem addColumnMenu;
+    private JMenuItem delColumnMenu;
+
+    private List<TableColumnView> columnViewList;
 
     public TableCreatorView(ClientController clientController) {
         super(WorkspaceType.TABLE_CREATOR);
@@ -22,11 +38,70 @@ public class TableCreatorView extends WorkspaceBase {
 
         setLayout(new BorderLayout());
         add(contentPanel);
-
         columns.setLayout(new BoxLayout(columns, BoxLayout.Y_AXIS));
 
-        columns.add(new TableColumnView());
-        columns.add(new TableColumnView());
-        columns.add(new TableColumnView());
+        columnViewList = new ArrayList<>();
+//        columnViewList.add(new TableColumnView());
+//        columnViewList.add(new TableColumnView());
+//
+//        columnViewList.forEach(columns::add);
+
+        initPopupMenus();
+    }
+
+    private void initPopupMenus() {
+        columnsPopupMenu = new JPopupMenu();
+
+        addColumnMenu = new JMenuItem("Add column");
+        addColumnMenu.addActionListener(this::onAddColumnMenuClicked);
+        columnsPopupMenu.add(addColumnMenu);
+
+        delColumnMenu = new JMenuItem("Delete column");
+        delColumnMenu.addActionListener(this::onDelColumnMenuClicked);
+        columnsPopupMenu.add(delColumnMenu);
+
+        columns.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                onColumnsMouseClicked(mouseEvent);
+            }
+        });
+    }
+
+    private void onAddColumnMenuClicked(ActionEvent actionEvent) {
+        TableColumnView tableColumnView = new TableColumnView();
+        columnViewList.add(tableColumnView);
+        columns.add(tableColumnView);
+
+        columnsScrollPane.revalidate();
+        columnsScrollPane.repaint();
+    }
+
+    private void onDelColumnMenuClicked(ActionEvent actionEvent) {
+        if (selectedColumn == null) {
+            return;
+        }
+
+        columnViewList.remove(selectedColumn);
+        columns.remove(selectedColumn);
+
+        columnsScrollPane.revalidate();
+        columnsScrollPane.repaint();
+    }
+
+    private void onColumnsMouseClicked(MouseEvent mouseEvent) {
+        if (!SwingUtilities.isRightMouseButton(mouseEvent)) {
+            return;
+        }
+
+        Component c = columns.getComponentAt(mouseEvent.getPoint());
+        if (c instanceof TableColumnView) {
+            selectedColumn = (TableColumnView) c;
+            delColumnMenu.setEnabled(true);
+        } else {
+            delColumnMenu.setEnabled(false);
+        }
+
+        columnsPopupMenu.show(columns, mouseEvent.getX(), mouseEvent.getY());
     }
 }
