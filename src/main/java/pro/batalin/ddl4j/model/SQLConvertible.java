2e80726f6456fa4e1a11ac6d49c8dbf58b12fd21
Kirill Batalin <kir55rus@gmail.com>
2017-06-05 22:07:12 +0700

    Del ddl4j src

--- src/main/java/pro/batalin/ddl4j/model/SQLConvertible.java
+++
@@ -1,7 +0,0 @@
-package pro.batalin.ddl4j.model;
-
-/**
- * Created by Kirill Batalin (kir55rus) on 08.05.17.
- */
-public interface SQLConvertible {
-}
--- src/main/java/pro/batalin/ddl4j/platforms/PlatformBaseImpl.java
+++
@@ -1,8 +0,0 @@
-package pro.batalin.ddl4j.platforms;
-
-/**
- * Created by Kirill Batalin (kir55rus) on 07.05.17.
- */
-abstract public class PlatformBaseImpl implements Platform {
-
-}
