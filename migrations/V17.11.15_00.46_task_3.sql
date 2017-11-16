84f3c5f4d947fef47283c7c24dc97a0dad16676b
Lobkov Ilya <ilobkov@themsteam.com>
2017-11-16 22:23:03 +0700

    Add task 3

---
+++ migrations/V17.11.15_00.46_task_3.sql
@@ -0,0 +1,3 @@
+INSERT INTO quest17.tasks (id, name, text, points, answer, visible, deadline, task, active) VALUES (3, 'Что в имени тебе моем?', '', 20, 'озимандия', 1, null, 'Чтобы попасть в убежище, нужно найти человека. Это вся информация, которую я нашел.
+
+<img src="/img/task_3.jpg">', 1);
