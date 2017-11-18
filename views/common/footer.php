8066cab2aa200535bc62ced2c580124de7b4e234
Kirill Batalin <kir55rus@gmail.com>
2017-11-18 23:00:40 +0700

    Upd

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
     <hr>
-    <a href="https://vk.com/nasdohuja2016">Посвящение ФИТ и ФИЯ 2016</a>
+    <a href="https://vk.com/fit_fija_2017">Посвящение ФИТ и ФИЯ 2017</a>
 </div>
