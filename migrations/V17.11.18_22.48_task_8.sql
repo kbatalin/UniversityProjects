72fd78f5232e04c9549bbc5c3d1ba50a384010b7
Kirill Batalin <kir55rus@gmail.com>
2017-11-18 22:48:43 +0700

    Add task 8

---
+++ migrations/V17.11.18_22.48_task_8.sql
@@ -0,0 +1,3 @@
+INSERT INTO quest17.tasks (id, name, text, points, answer, visible, deadline, task, active) VALUES
+  (8, 'Спизженный DUMP', '', 20, 'New Mexico', 1, NULL,
+   'Привет! Я с ФИТа с 147 курса видел тебя в универе 183 числа, ты мне очень понравилась. Лови фотку….', 1);
