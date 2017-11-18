3aece6445cbd84502992767849025cb8ffdcbebc
Kirill Batalin <kir55rus@gmail.com>
2017-11-18 21:59:36 +0700

    Add task 7

---
+++ migrations/V17.11.18_21.58_task_7.sql
@@ -0,0 +1,6 @@
+INSERT INTO quest17.tasks (id, name, text, points, answer, visible, deadline, task, active) VALUES
+  (7, 'Все, что видим мы, видимость только одна или главное не то, что снаружи', '', 20,
+   'Eita te mau auvaha e taama i teia tiare matie.', 1, NULL, 'Из этой картинки нужно выудить максимум информации за ограниченное количество времени
+А это точно картинка?...
+
+<a href="/files/something.jpg">Картинка</a>', 1);
