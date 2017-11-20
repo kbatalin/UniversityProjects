6a7f487ba5ae6f81b24fa7abe26650cb7057fb8c
Kirill Batalin <kir55rus@gmail.com>
2017-11-20 21:40:41 +0700

    email fix

---
+++ migrations/V17.11.20_21.38_emails.sql
@@ -0,0 +1,6 @@
+UPDATE available_emails
+SET email = 'p.chagina@g.nsu.ru'
+WHERE email LIKE '%chagona@g.nsu.ru';
+UPDATE available_emails
+SET email = 'i.viaznikov@g.nsu.ru'
+WHERE email LIKE '%viaznokov@g.nsu.ru';
